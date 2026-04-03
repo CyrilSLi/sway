fb220f7e74f1fccdda0c339216c404c632279144
Cyril Li <cyrils.li@icloud.com>
2026-04-02 20:03:51 -0500

    disable swaybar_command (personal preference)

--- sway/commands/bar/swaybar_command.c
+++ sway/commands/bar/swaybar_command.c
@@ -4,6 +4,8 @@
 #include "stringop.h"
 
 struct cmd_results *bar_cmd_swaybar_command(int argc, char **argv) {
+	return cmd_results_new(CMD_SUCCESS, NULL); // Disable swaybar_command (personal preference)
+
 	struct cmd_results *error = NULL;
 	if ((error = checkarg(argc, "swaybar_command", EXPECTED_AT_LEAST, 1))) {
 		return error;
